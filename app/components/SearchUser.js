7c07e01656cbc411d4722b84972e443727fdbb66
Daniel Galvão <galvaodev@gmail.com>
2016-06-25 00:06:55 -0300

    add services to get github infos

--- app/components/SearchUser.js
+++ app/components/SearchUser.js
@@ -1,9 +1,16 @@
 var React = require('react');
+var GitHubUser = require('../services/GitHubUser');
 
 var SearchUser = React.createClass({
   handleSubmit: function(e){
+
     e.preventDefault();
-    console.log(this.refs.username.value);
+    GitHubUser.getByUsername(this.refs.username.value).then(function(resp){
+      this.props.updateUser(resp.data);
+    }.bind(this));
+    GitHubUser.getReposByUsername(this.refs.username.value).then(function(resp){
+      this.props.updateRepos(resp.data);
+    }.bind(this));
   },
   render: function(){
     return(
